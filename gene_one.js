4d9c95944d3985e654ef68c885af4a798be31c16
Joe-W <80954812+J-Wll@users.noreply.github.com>
2024-10-02 21:31:56 +0100

    Fixed sliders more. Properly resets without page refresh, added triangle height slider, Tweaks

--- gene_one.js
+++ gene_one.js
@@ -1,6 +1,6 @@
 
-c2 = 255;
-c1 = 0;
+let c2 = 255;
+let c1 = 0;
 let length = 250;
 let moveOffset = 20;
 let increment = 0;
@@ -10,9 +10,19 @@ let ran3 = 0;
 let ranfactor = 1;
 let runtime = 300 // 300 actual value higher for testing
 let opacity = 0.6
+let ofst = 0.5
+let decrease = true;
+let triHeightFactor = 1;
+// TODO: Make moving a checkbox
+let moving = true;
+
+const canvas = document.querySelector('.my_canvas');
+const width = canvas.width = window.innerWidth - 500;
+const height = canvas.height = window.innerHeight - 100;
+const ctx = canvas.getContext('2d');
 
 runtimeSlider = document.querySelector("#sld_runtime");
-runtimeSlider.value = ranfactor;
+runtimeSlider.value = runtime;
 runtimeSlider.onchange = function () {
   runtime = this.value;
 }
@@ -27,67 +37,79 @@ ranfactorSlider = document.querySelector("#sld_randomization");
 ranfactorSlider.value = ranfactor;
 ranfactorSlider.onchange = function () {
   ranfactor = this.value;
-  increment += Math.floor(Math.random() * (1.5 * ranfactor)) + 1;
-  ran2 += Math.floor(Math.random() * (3 * ranfactor)) + 1;
-  ran3 += Math.floor(Math.random() * (50 * ranfactor)) + 1;
-
+  increment = Math.floor(Math.random() * (1.5 * ranfactor)) + 1;
+  ran2 = Math.floor(Math.random() * (3 * ranfactor)) + 1;
+  ran3 = Math.floor(Math.random() * (50 * ranfactor)) + 1;
 }
-// let runtime = document.querySelector("sld_runtime").value;
-// let opacity = document.querySelector("sld_transparency").value;
-// let ranfactor = document.querySelector("sld_randomization").value;
 
-// a_runtime.oninput = function() {
-//   output.innerHTML = this.value;
-// }
-
-
-increment += Math.floor(Math.random() * (1.5 * ranfactor)) + 1;
-ran2 += Math.floor(Math.random() * (3 * ranfactor)) + 1;
-ran3 += Math.floor(Math.random() * (50 * ranfactor)) + 1;
-
-
-// const canvas = document.querySelector('.my_canvas');
-// var width = canvas.width = window.innerWidth - 300;
-// var height = canvas.height = window.innerHeight - 200;
-// var ctx = canvas.getContext('2d');
-// ctx.fillStyle = 'rgb(40,40,40)';
-// ctx.fillRect(0, 0, width, height);
-// ctx.translate(width / 2, height / 2);
+triSlider = document.querySelector("#sld_triheight");
+triSlider.value = triHeightFactor;
+triSlider.onchange = function () {
+  triHeightFactor = this.value;
+}
 
 function degToRad(degrees) {
   return degrees * Math.PI / 160;
 }
 
+function canvasReset() {
+  ctx.reset()
+  run_count = 0;
+  c2 = 255;
+  c1 = 0;
+  length = 250;
+  moveOffset = 20;
+  increment = 0;
+  ofst = 0.5;
+  ran2 = 0;
+  ran3 = 0;
+  decrease = true;
 
+  ctx.translate(width / 2, height / 2);
+  ctx.fillStyle = 'rgb(40,40,40)';
+  ctx.fillRect(0, 0, width, height);
+  ctx.clearRect(0, 0, canvas.width, canvas.height)
+  increment += Math.floor(Math.random() * (1.5 * ranfactor)) + 1;
+  ran2 += Math.floor(Math.random() * (3 * ranfactor)) + 1;
+  ran3 += Math.floor(Math.random() * (50 * ranfactor)) + 1;
 
-
-const canvas = document.querySelector('.my_canvas');
-const width = canvas.width = window.innerWidth - 500;
-const height = canvas.height = window.innerHeight - 100;
-const ctx = canvas.getContext('2d');
-ctx.fillStyle = 'rgb(40,40,40)';
-ctx.fillRect(0, 0, width, height);
-ctx.translate(width / 2, height / 2);
+  gen()
+}
 
 function gen() {
   run_count += 1
-  if (run_count >= runtime) { // run_count 300 neat number for just the circle in the middle. 900 decent for second monitor 
-    run_count = 0;
-    // window.location.href = "gene_one.html"
+
+  const triHeight = (length / 2 * Math.tan(degToRad(60)) * triHeightFactor);
+  ofst = 0.5
+  // this toggle replaces gen2
+  if (moving) {
+    ofst += increment / 2
+    moveOffset += ofst;
   }
-  let i = 1;
-  let ofst = 0.5
   ctx.fillStyle = `rgba(${c1}, 100, ${c2}, ${opacity})`
   ctx.beginPath();
   ctx.moveTo(moveOffset, moveOffset);
   ctx.lineTo(moveOffset + length, moveOffset);
-  let triHeight = length / 2 * Math.tan(degToRad(60));
   ctx.lineTo(moveOffset + (length / 2), moveOffset + triHeight);
   ctx.lineTo(moveOffset, moveOffset);
   ctx.fill();
-  ofst += increment / 2
-  length -= ran2;
-  moveOffset += ofst;
+
+  ctx.rotate(degToRad(ran3));
+
+  // this system stops the triangles from becoming insanely large because its not interesting to have the whole screen be a triangle 
+  if (decrease) {
+    length -= ran2;
+  } else {
+    length += ran2;
+  }
+
+  if (length < -400) {
+    decrease = false;
+  }
+  if (length > 400) {
+    decrease = true;
+  }
+
   if (c1 >= 150) {
     c1 -= 200;
     c2 += 200;
@@ -96,9 +118,19 @@ function gen() {
     c1 += 200;
     c2 -= 200;
   }
-  ctx.rotate(degToRad(ran3));
-  window.requestAnimationFrame(gen)
+
+  if (run_count >= runtime) {
+    setTimeout(() => {
+      canvasReset();
+    }, 500)
+  }
+  else {
+    window.requestAnimationFrame(gen)
+    console.log(triHeight, c1, c2, opacity, moveOffset, length, ofst, "AAA", increment, ranfactor, ran2, ran3, run_count)
+  }
+
 }
 
-gen()
+canvasReset();
+gen();
 
